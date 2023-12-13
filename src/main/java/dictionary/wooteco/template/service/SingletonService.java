62cf76282c16ac6f620a7b93bedba4f54ed11fdf
jinny-l <jinny.l.dev@gmail.com>
2023-12-13 22:09:12 +0900

    feat: `SingletonService` 템플릿 코드 구현

--- src/main/java/dictionary/wooteco/template/service/SingletonService.java
+++ src/main/java/dictionary/wooteco/template/service/SingletonService.java
@@ -2,12 +2,12 @@ package dictionary.wooteco.template.service;
 
 public class SingletonService {
 
-    private static final SingletonService SINGLETON_SERVICE = new SingletonService();
+    private static final SingletonService INSTANCE = new SingletonService();
 
     private SingletonService() {
     }
 
     public static SingletonService getInstance() {
-        return SINGLETON_SERVICE;
+        return INSTANCE;
     }
 }
